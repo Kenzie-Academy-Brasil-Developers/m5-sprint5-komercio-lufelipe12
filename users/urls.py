07dba9f491f76aa42bf3301ca0583cd1b7037ca4
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-06-27 16:40:27 -0300

    Feat: organize code

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from .views import UserView, UserLoginView, UserRetrieveUpdateDestroyAPIView, ListNumberOfUsersView
+from .views import (
+    UserView,
+    UserLoginView,
+    UserRetrieveUpdateDestroyAPIView,
+    ListNumberOfUsersView,
+)
 
 urlpatterns = [
     path("accounts/", UserView.as_view()),
